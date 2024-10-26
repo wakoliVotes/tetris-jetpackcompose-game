c33b0be74f63137654e018f2804863e38643cc2f
wakoiVotes <votesdw@yahoo.com>
2024-10-26 18:15:49 +0300

    success

--- app/src/main/java/com/example/tetrisjet/ui/theme/game/TetrisGameUtils.kt
+++ app/src/main/java/com/example/tetrisjet/ui/theme/game/TetrisGameUtils.kt
@@ -1,2 +1,74 @@
 package com.example.tetrisjet.ui.theme.game
 
+import androidx.compose.ui.graphics.Color
+import com.example.tetrisjet.ui.theme.presentation.Pair
+
+fun calculateScore(linesDestroyed: Int) = when (linesDestroyed) {
+    1 -> 100
+    2 -> 300
+    3 -> 700
+    4 -> 1500
+    else -> 0
+}
+
+val shapeVariants = listOf(
+    listOf(Pair(0, -1), Pair(0, 0), Pair(-1, 0), Pair(-1, 1)) to Color(0xFF3D76B5),
+    listOf(Pair(0, -1), Pair(0, 0), Pair(1, 0), Pair(1, 1)) to Color(0xFFA369B8),
+    listOf(Pair(0, -1), Pair(0, 0), Pair(0, 1), Pair(0, 2)) to Color(0xFFFF0128),
+    listOf(Pair(0, 1), Pair(0, 0), Pair(0, -1), Pair(1, 0)) to Color(0xFF43D462),
+    listOf(Pair(0, 0), Pair(-1, 0), Pair(0, -1), Pair(-1, -1)) to Color(0xFFFBCD05),
+    listOf(Pair(-1, -1), Pair(0, -1), Pair(0, 0), Pair(0, 1)) to Color(0xFF53B1FD),
+    listOf(Pair(1, -1), Pair(0, -1), Pair(0, 0), Pair(0, 1)) to Color(0xFFEDEAE9)
+)
+
+operator fun Pair<Int, Int>.plus(pair: Pair<Int, Int>): Pair<Int, Int> =
+    Pair(first + pair.first, second + pair.second)
+
+operator fun Pair<Int, Int>.div(pair: Pair<Int, Int>): Pair<Int, Int> =
+    Pair(first / pair.first, second / pair.second)
+
+operator fun Pair<Int, Int>.times(pair: Pair<Int, Int>): Pair<Int, Int> =
+    Pair(first / pair.first, second * pair.second)
+
+fun <T> MutableList<List<T>>.updates(i: Int, j: Int, item: T): MutableList<List<T>> = apply {
+    this[j] = this[j].toMutableList().apply {
+        this[i] = item
+    }
+}
+
+
+val <T> List<List<T>>.multiSize get() = (firstOrNull()?.size ?: 0) to size
+
+fun TetrisBlock.createProjection(blocks: Board): TetrisBlock {
+    var projection = this
+    while (projection.move(0 to 1).isValid(blocks)) {
+        projection = projection.move(0 to 1)
+    }
+    return projection
+}
+
+
+fun Board.modifyBlocks(block: TetrisBlock): Pair<Board, Int> {
+    val size = multiSize
+    var newBoard = this.toMutableList()
+    var destroyedRows = 0
+    block.cordinates.forEach {
+        newBoard = newBoard.update(it.first, it.second, block.color)
+    }
+    if (newBoard.removeAll { row -> row.all { it != Color.Unspecified}}) {
+        destroyedRows = size.second - newBoard.size
+        newBoard.addAll(0, (0 until destroyedRows).map { (0 until size.first).map { Color.Unspecified } })
+    }
+    return newBoard to destroyedRows
+}
+
+
+fun TetrisBlock.isValid(blocks: Board): Boolean {
+    val size = blocks.multiSize
+    return cordinates.none {
+        it.first <0 || it.first > size.first -1 || it.second > size.second -1 ||
+                blocks[it.second][it.first] != Color.Unspecified
+    }
+}
+
+
