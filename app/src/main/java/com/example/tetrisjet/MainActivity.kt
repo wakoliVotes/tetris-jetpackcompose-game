661a5b8f4fb615767445d8ad84eea8160a1c0dc8
wakoiVotes <votesdw@yahoo.com>
2024-10-31 08:58:29 +0300

    MainActivity

--- app/src/main/java/com/example/tetrisjet/MainActivity.kt
+++ app/src/main/java/com/example/tetrisjet/MainActivity.kt
@@ -4,14 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tetrisjet.ui.theme.TetrisjetTheme
+import com.example.tetrisjet.ui.theme.presentation.TetrisGame
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,11 +19,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             TetrisjetTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface (color = MaterialTheme.colorScheme.background) {
+                    TetrisGame()
                 }
             }
         }
